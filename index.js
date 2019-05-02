0b152b1091a4c22098ee2efd00d2484d5a3fea2a
Julian <julian@juliantomlindesign.com>
2019-05-02 23:32:12 +0000

    finished test

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  let number = toN
+  if (number <= fromN){
+    return toN
+  }
+
+  return number + sum(fromN ,(number - 1))
 }
 
 module.exports = sum;
